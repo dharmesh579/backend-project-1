2ddb23784df8e7aab544037fcc4d7e735f571b17
dharmesh579 <dharmeshsasikiran@gmail.com>
2026-02-28 21:20:18 +0530

    set up app.js in the src

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-require("dotenv").config()
-const main = require("./db/index.js")
+require("dotenv").config();
+const main = require("./db/index.js");
+const app = require("./app.js");
+const port = process.env.PORT || 6000;
 
-main()
+main();
+
+app.listen(port, () => {
+  console.log(`Server started on Port ${port}`);
+});

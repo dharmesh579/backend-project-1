cc9b0862383d82bfd16397d0132edbd9b503cda8
dharmesh579 <dharmeshsasikiran@gmail.com>
2026-03-01 18:03:09 +0530

    Add multer middleware and cloudinary utililty for file uploading

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./tmp/my-uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + "-" + Date.now());
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+module.exports = upload;
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDNAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+module.exports = uploadOnCloudinary;

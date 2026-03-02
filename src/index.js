275ee2f854c66fee2fbc0ee4649c06fe0d10e018
dharmesh579 <dharmeshsasikiran@gmail.com>
2026-03-02 10:08:32 +0530

    minor error fixes and tested register,login,logout controllers

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const main = require("./db/index.js");
 const app = require("./app.js");
 const port = process.env.PORT || 6000;
 
-main();
+main()
+
 
 app.listen(port, () => {
   console.log(`Server started on Port ${port}`);
